2c5ad050d594c395a0f8733b0d9eec52e23c0969
Monosen <edinson.bolanos00@usc.edu.co>
2022-10-30 20:24:04 -0500

    feat(api): adding database parameter to movie schema

--- apps/api/src/app/movie/entities/movie.entity.ts
+++ apps/api/src/app/movie/entities/movie.entity.ts
@@ -1,1 +1,37 @@
-export class Movie {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Movie extends Document {
+  @Prop({
+    unique: true,
+    index: true,
+    required: true,
+  })
+  name: string;
+
+  @Prop({
+    type: [String],
+    required: true,
+  })
+  categories: string[];
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  description: string;
+
+  @Prop({
+    type: Number,
+    required: true,
+  })
+  punctuation: number;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  image: string;
+}
+
+export const MovieSchema = SchemaFactory.createForClass(Movie);
