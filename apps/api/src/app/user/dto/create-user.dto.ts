5a38f196d3a4653fe1c84e0c91e6ff7ff8c35088
Monosen <edinson.bolanos00@usc.edu.co>
2022-10-31 18:58:17 -0500

    feat(api/user): adding create-user.dto.ts

--- apps/api/src/app/user/dto/create-user.dto.ts
+++ apps/api/src/app/user/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { IsArray, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(1)
+  username: string;
+
+  @IsString()
+  @MinLength(1)
+  password: string;
+
+  @IsString()
+  @MinLength(1)
+  address: string;
+
+  @IsString()
+  @MinLength(1)
+  country: string;
+
+  @IsString({ each: true })
+  @IsArray()
+  role: string[];
+}
