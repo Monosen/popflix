f4cd858d0109e7c5eafb907cc3fe39cf01082de8
Monosen <edinson.bolanos00@usc.edu.co>
2022-10-30 21:02:28 -0500

    feat(api/movie): adding crud of the functional movie scheme

--- apps/api/src/app/movie/movie.service.ts
+++ apps/api/src/app/movie/movie.service.ts
@@ -1,26 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
 
 @Injectable()
 export class MovieService {
-  create(createMovieDto: CreateMovieDto) {
-    return 'This action adds a new movie';
+  constructor(
+    @InjectModel(Movie.name)
+    private readonly movieModel: Model<Movie>
+  ) {}
+
+  async create(createMovieDto: CreateMovieDto) {
+    try {
+      const movie = await this.movieModel.create(createMovieDto);
+
+      return movie;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all movie`;
+  async findAll() {
+    try {
+      const movies = await this.movieModel.find().lean();
+
+      return movies;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} movie`;
+  async findOne(id: number) {
+    const movie = await this.movieModel.findById(id).lean();
+
+    if (!movie) {
+      throw new BadRequestException(`Movie not found`);
+    }
+
+    return movie;
   }
 
   update(id: number, updateMovieDto: UpdateMovieDto) {
-    return `This action updates a #${id} movie`;
+    const movie = this.movieModel.findByIdAndUpdate(id, updateMovieDto).lean();
+
+    if (!movie) {
+      throw new BadRequestException(`Movie not found`);
+    }
+
+    return movie;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} movie`;
+  async remove(id: number) {
+    const movie = await this.movieModel.findByIdAndDelete(id);
+
+    if (!movie) {
+      throw new BadRequestException(`Movie not found`);
+    }
+  }
+
+  private handleExceptions(error: any) {
+    console.log(error);
+    throw new InternalServerErrorException(`Check server logs`);
   }
 }
