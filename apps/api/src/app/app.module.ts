e1171b4d50793272322dc0e804bc7929703aabc3
Monosen <edinson.bolanos00@usc.edu.co>
2022-10-30 23:07:46 -0500

    feat(api/user): adding the user schema

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MovieModule } from './movie/movie.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URI), MovieModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    MovieModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
