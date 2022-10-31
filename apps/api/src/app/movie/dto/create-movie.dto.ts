84dd209cc2ea3bc4baf1238f99ba914eb1bf7fe2
Monosen <edinson.bolanos00@usc.edu.co>
2022-10-30 20:22:52 -0500

    feat(api): adding create-movie.dto.ts

--- apps/api/src/app/movie/dto/create-movie.dto.ts
+++ apps/api/src/app/movie/dto/create-movie.dto.ts
@@ -1,1 +1,31 @@
-export class CreateMovieDto {}
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateMovieDto {
+  // isString, Minlenth 1
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @IsString({ each: true })
+  @IsArray()
+  categories: string[];
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  @IsPositive()
+  punctuation: number;
+
+  @IsString()
+  @IsNotEmpty()
+  image: string;
+}
