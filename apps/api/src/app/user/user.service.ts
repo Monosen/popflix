0b91de5d0464304e92bfb06228c4f8c3b7a6f834
Monosen <edinson.bolanos00@usc.edu.co>
2022-10-31 21:32:02 -0500

    feat(api/user): adding user functionality

--- apps/api/src/app/user/user.service.ts
+++ apps/api/src/app/user/user.service.ts
@@ -1,26 +1,89 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import * as argon from 'argon2';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>
+  ) {}
+
+  async signUp(createUserDto: CreateUserDto) {
+    createUserDto.password = await argon.hash(createUserDto.password);
+
+    const user = await this.userModel.create(createUserDto);
+
+    return { ...user, password: undefined };
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async signIn(updateUserDto: UpdateUserDto) {
+    const user = await this.userModel.findOne({
+      username: updateUserDto?.username,
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const isPasswordValid = await argon.verify(
+      user.password,
+      updateUserDto.password
+    );
+
+    if (!isPasswordValid) {
+      throw new Error('Invalid password');
+    }
+
+    return { ...user, password: undefined };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findAll() {
+    try {
+      const users = await this.userModel.find().lean();
+
+      return users;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOne(id: number) {
+    const user = await this.userModel.findById(id).lean();
+
+    if (!user) {
+      throw new Error(`User not found`);
+    }
+
+    return user;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.userModel
+      .findByIdAndUpdate(id, updateUserDto)
+      .lean();
+
+    if (!user) {
+      throw new Error(`User not found`);
+    }
+
+    return user;
+  }
+
+  async remove(id: number) {
+    const user = await this.userModel.findByIdAndDelete(id);
+
+    if (!user) {
+      throw new Error(`User not found`);
+    }
+  }
+
+  private handleExceptions(error: any) {
+    console.log(error);
+    throw new InternalServerErrorException(`Check server logs`);
   }
 }
