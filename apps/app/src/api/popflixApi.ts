5a8bb5fd24a12bee78c180d1880f29546085a948
Monosen <edinson.bolanos00@usc.edu.co>
2022-12-04 01:19:56 -0500

    feat(app): add authorization headers in axios

--- apps/app/src/api/popflixApi.ts
+++ apps/app/src/api/popflixApi.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 export const popflixApi = axios.create({
   baseURL: 'http://localhost:3333/api',
+  headers: {
+    Authorization: `Bearer ${Cookies.get('token')}`,
+  },
 });
